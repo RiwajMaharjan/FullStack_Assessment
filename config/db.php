818f723e28415d2084f8bad280d5304ec3146626
RiwajMaharjan <np03cs4a240114@heraldcollege.edu.np>
2026-01-24 14:54:22 +0545

    structure made

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "fitness_club";
+
+    try {
+        $pdo = new PDO("mysql:host=$servername;dbname=$database", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        // echo "connected success!";
+    } catch (\Throwable $th) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
